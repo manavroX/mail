e9bbfebb76f28d9d407dce7aeaf622b685384508
manavroX <38128162+manavroX@users.noreply.github.com>
2021-04-10 19:12:41 +0530

    wdwdbkb

--- mail.php
+++ mail.php
@@ -1,10 +1,11 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
-
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
-
-// send email
-mail("manav.bhaichandani3@gmail.com","My subject",$msg);
-?>
+$to_email = "manav.bhagchandani3@gmail.com";
+$subject = "Simple Email Test via PHP";
+$body = "Hi,nn This is test email send by PHP Script";
+$headers = "From: manav.bhagchandani3@gmail.com";
+ 
+if (mail($to_email, $subject, $body, $headers)) {
+    echo "Email successfully sent to $to_email...";
+} else {
+    echo "Email sending failed...";
+}
